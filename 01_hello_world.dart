8d13cc965f7660d4a91d9b00a611f4fc9ab973b0
Alan Bersia <alanbersia@gmail.com>
2023-04-27 12:38:24 -0300

    Primer subida

---
+++ 01_hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+  // var myName = 'Alan';
+  // String myName = 'Alan';
+  // late final myName;
+  const myName = 'Alan';
+  
+  
+  print('Hola $myName');
+  print('Hola ${ myName.toUpperCase() }');
+  print('Hola ${ 1 + 1 }'); 
+}
